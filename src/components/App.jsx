1ca95ff560ab555bbab9407046d8de5f93bc4636
IsabelCollado <icolladoherrera@gmail.com>
2024-05-16 09:24:29 +0200

    Add install starter kit rect+vite

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+// Fichero src/component/App.js
+import '../styles/App.scss';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Starter Kit Templace React+Vite</h1>
+    </div>
+  );
+};
+
+export default App;
